ef4e69a87fc86bfe2c4f9eedaeff46e7ff1a4a68
sebs macOS <sam.skinr@gmail.com>
2024-01-29 11:11:39 -0500

    added buffer demo

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,6 +1,6 @@
 use std::sync::atomic::AtomicBool;
 
-use wgpu::util::DeviceExt;
+use wgpu::util::{BufferInitDescriptor, DeviceExt};
 
 use crate::gfx;
 pub struct Game<'r>
@@ -19,6 +19,15 @@ impl<'r> Game<'r>
 
     pub fn enter_tick_loop(&self, should_stop: &AtomicBool)
     {
+        let all_u8s_rev: [u8; u8::MAX as usize + 1] =
+            std::array::from_fn(|i| u8::wrapping_sub(1, i as u8).wrapping_sub(2));
+
+        self.renderer.create_buffer_init(&BufferInitDescriptor {
+            label:    Some("wgpu test buffer"),
+            contents: &all_u8s_rev,
+            usage:    wgpu::BufferUsages::STORAGE
+        });
+
         while !should_stop.load(std::sync::atomic::Ordering::Acquire)
         {}
     }
