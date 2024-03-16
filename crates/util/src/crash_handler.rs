ce4aa3f77980e8bc95cf8b211678f8d37ce2ada6
sebs macOS <sam.skinr@gmail.com>
2024-03-15 20:35:15 -0400

    sync

--- crates/util/src/crash_handler.rs
+++ crates/util/src/crash_handler.rs
@@ -1,81 +1,58 @@
 use std::any::Any;
-use std::collections::HashMap;
-use std::panic::{RefUnwindSafe, UnwindSafe};
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::*;
-use std::sync::{Arc, Mutex};
-use std::thread::JoinHandle;
+use std::fmt::Display;
+use std::panic::UnwindSafe;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::Mutex;
 
-use chrono::Utc;
-
-use crate::Uuid;
-
-pub struct CrashHandlerManagedLoopSpawner
+pub struct CrashHandlerManagedLoopSpawner<'h>
 {
-    id:             Uuid,
-    name:           String,
-    owning_handler: Arc<CrashHandler>
+    handler: &'h CrashHandler
 }
 
-impl Drop for CrashHandlerManagedLoopSpawner
-{
-    fn drop(&mut self)
-    {
-        self.owning_handler.acknowledge_termination(self.id);
-    }
-}
+type ContinueLoopingFunc = dyn Fn() -> bool;
+type TerminateLoopsFunc = dyn Fn();
 
-impl CrashHandlerManagedLoopSpawner
+impl CrashHandlerManagedLoopSpawner<'_>
 {
-    pub fn enter_oneshot<R>(self, func: impl FnOnce() -> R + UnwindSafe) -> Option<R>
+    pub fn enter_oneshot<R, F>(&self, name: String, func: F) -> R
+    where
+        F: FnOnce() -> R + UnwindSafe
     {
-        match std::panic::catch_unwind(func)
+        match std::panic::catch_unwind(|| func(&|| self.handler.should_loops_iterate()))
         {
-            Ok(r) => Some(r),
-            Err(e) =>
+            Ok(r) => r,
+            Err(payload) =>
             {
-                self.owning_handler.acknowledge_crash(self.id, e);
-                None
+                std::panic::panic_any(CrashInfo {
+                    thread_name: name,
+                    panic_time: std::time::Instant::now(),
+                    payload
+                });
             }
         }
     }
 
-    pub fn enter_managed_loop(self, func: impl Fn() -> TerminationResult + RefUnwindSafe)
-    {
-        if let Err(e) = std::panic::catch_unwind(|| {
-            while self.owning_handler.should_loops_iterate()
-            {
-                match func()
-                {
-                    TerminationResult::RequestIteration =>
-                    {}
-                    TerminationResult::Terminate => break
-                }
+    // pub fn enter_managed_thread
 
-                self.owning_handler.acknowledge_iteration(self.id);
-            }
-        })
-        {
-            self.owning_handler.acknowledge_crash(self.id, e);
-        }
-    }
+    // pub fn enter_managed_loop(&self, func: impl Fn() -> TerminationResult +
+    // RefUnwindSafe) {
+    //     if let Err(e) = std::panic::catch_unwind(|| {
+    //         while self.owning_handler.should_loops_iterate()
+    //         {
+    //             match func()
+    //             {
+    //                 TerminationResult::RequestIteration =>
+    //                 {}
+    //                 TerminationResult::Terminate => break
+    //             }
 
-    pub fn enter_managed_thread(
-        self,
-        func: impl Fn() -> TerminationResult + UnwindSafe + RefUnwindSafe + Send + 'static
-    )
-    {
-        let name = self.name.clone();
-        let crash_handler = self.owning_handler.clone();
-
-        crash_handler.register_join_handle(
-            self.id,
-            std::thread::Builder::new()
-                .name(self.name.clone())
-                .spawn(move || self.enter_managed_loop(func))
-                .unwrap_or_else(|_| panic!("Failed to spawn thread {}", name))
-        );
-    }
+    //             self.owning_handler.acknowledge_iteration(self.id);
+    //         }
+    //     })
+    //     {
+    //         self.owning_handler.acknowledge_crash(self.id, e);
+    //     }
+    // }
 }
 
 pub enum TerminationResult
@@ -85,153 +62,84 @@ pub enum TerminationResult
 }
 
 type PanicPayload = Box<dyn Any + Send>;
-pub trait CrashHandlerFunction = Fn(CrashLog) + Send + 'static;
 
 pub struct CrashHandler
 {
-    handle_crash_function:  Mutex<Option<Box<dyn CrashHandlerFunction>>>,
-    should_loops_iterate:   AtomicBool,
-    loop_acknowledge_times: Mutex<HashMap<Uuid, chrono::DateTime<Utc>>>,
-    loop_names:             Mutex<HashMap<Uuid, String>>,
-    thread_join_handles:    Mutex<Option<HashMap<Uuid, JoinHandle<()>>>>,
-    panic_data:             Mutex<Option<HashMap<Uuid, (std::time::Instant, PanicPayload)>>>
+    crash_receiver: Receiver<CrashInfo>,
+    crash_sender:   Sender<CrashInfo>
 }
 
 impl CrashHandler
 {
     #[allow(clippy::new_without_default)]
-    pub fn new(crash_func: impl CrashHandlerFunction) -> Arc<CrashHandler>
+    pub fn new() -> CrashHandler
     {
-        Arc::new(CrashHandler {
-            handle_crash_function:  Mutex::new(Some(Box::new(crash_func))),
-            should_loops_iterate:   AtomicBool::new(true),
-            loop_acknowledge_times: Mutex::new(HashMap::new()),
-            loop_names:             Mutex::new(HashMap::new()),
-            thread_join_handles:    Mutex::new(Some(HashMap::new())),
-            panic_data:             Mutex::new(Some(HashMap::new()))
-        })
-    }
+        let (tx, rx) = std::sync::mpsc::channel();
 
-    pub fn into_guarded_scope(&self, func: FnOnce() -> )
-    {
-
-    }
-
-    pub fn handle_crash(&self)
-    {
-        // no threads have crashed
-        if self.panic_data.lock().unwrap().unwrap().is_empty()
-        {
-            return;
+        CrashHandler {
+            crash_sender:   tx,
+            crash_receiver: rx
         }
+    }
 
-        self.should_loops_iterate.store(false, SeqCst);
-
-        self.thread_join_handles
-            .lock()
-            .unwrap()
-            .take()
-            .unwrap()
-            .into_iter()
-            .for_each(|(_, handle)| {
-                let _ = handle.join();
-            });
-
-        let mut panicked_threads = self
-            .panic_data
-            .lock()
-            .unwrap()
-            .take()
-            .unwrap()
-            .into_iter()
-            .collect::<Vec<_>>();
-
-        panicked_threads.sort_by(|l, r| l.1.0.cmp(&r.1.0));
-
-        self.crash_log.lock().unwrap() = CrashLog::new(panicked_threads.into_iter().map(|f| ThreadCrashLog { thread_name: todo!(), panic_time: todo!(), payload: todo!() }))
-        
-        std::panic::panic_any("Crash::handle_crash() Forcing Unwind!");
+    pub fn into_guarded_scope(
+        &self,
+        func: impl FnOnce(&CrashHandlerManagedLoopSpawner) + UnwindSafe
+    )
+    {
+        if let Err(e) = std::panic::catch_unwind(|| {
+            func(&CrashHandlerManagedLoopSpawner {
+                handler: self
+            })
+        })
+        {
+            match e.downcast::<CrashInfo>()
+            {
+                Ok(crash_info) => self.crash_sender.send(*crash_info).unwrap(),
+                Err(e) => std::panic::panic_any(e)
+            }
+        }
     }
 
     pub fn should_loops_iterate(&self) -> bool
     {
-        self.should_loops_iterate.load(SeqCst)
+        todo!()
     }
 
-    pub fn create_handle(self: Arc<Self>, name: String) -> CrashHandlerManagedLoopSpawner
+    pub fn
+
+    pub fn finish(self)
     {
-        let id = Uuid::new();
-        self.loop_acknowledge_times
-            .lock()
-            .unwrap()
-            .insert(id, chrono::Utc::now());
-        self.loop_names.lock().unwrap().insert(id, name.clone());
+        std::mem::drop(self.crash_sender);
 
-        CrashHandlerManagedLoopSpawner {
-            id,
-            name,
-            owning_handler: self
-        }
-    }
+        let frames = self
+            .crash_receiver
+            .iter()
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev()
+            .collect::<Vec<_>>();
 
-    pub fn acknowledge_iteration(&self, id: Uuid)
-    {
-        self.loop_acknowledge_times
-            .lock()
-            .unwrap()
-            .insert(id, chrono::Utc::now());
-    }
-
-    pub fn register_join_handle(&self, id: Uuid, join_handle: JoinHandle<()>)
-    {
-        assert!(
-            self.thread_join_handles
-                .lock()
-                .unwrap()
-                .as_mut()
-                .expect("Crash has already occurred!")
-                .insert(id, join_handle)
-                .is_none()
-        )
-    }
-
-    pub fn acknowledge_crash(&self, id: Uuid, panic_data: PanicPayload)
-    {
-        self.should_loops_iterate.store(false, SeqCst);
-
-        assert!(
-            self.panic_data
-                .lock()
-                .unwrap()
-                .as_mut()
-                .expect("Crash has already occurred!")
-                .insert(id, (std::time::Instant::now(), panic_data))
-                .is_none()
-        )
-    }
-
-    pub fn acknowledge_termination(&self, id: Uuid)
-    {
-        self.loop_acknowledge_times.lock().unwrap().remove(&id);
-        self.loop_names.lock().unwrap().remove(&id);
-        self.thread_join_handles.lock().unwrap().as_mut().unwrap().remove(&id);
-        self.panic_data.lock().unwrap().as_mut().unwrap().remove(&id);
+        frames.into_iter().for_each(|f| log::error!("{f}"));
     }
 }
 
-
-struct ThreadCrashLog {
+pub struct CrashInfo
+{
     thread_name: String,
-    panic_time: std::time::Instant,
+    panic_time:  std::time::Instant,
     // TODO: backtrace
-    payload: Box<PanicPayload>
+    payload:     PanicPayload
 }
 
-struct CrashLog {}
-
-impl CrashLog {
-    pub fn new(threads: impl IntoIterator<Item = ThreadCrashLog>)
+impl Display for CrashInfo
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
     {
-        
+        writeln!(
+            f,
+            "{} has crashed @ {:?} with message {:?}",
+            self.thread_name, self.panic_time, self.payload
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@
 #![feature(trait_upcasting)]
 #![feature(effects)]
 
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::*;
 use std::sync::{Arc, Mutex};
 
 fn main()
@@ -23,93 +21,49 @@ fn main()
     *util::access_global_thread_pool().write().unwrap() = Some(util::ThreadPool::new());
 
     let crash_handler = util::CrashHandler::new();
-    // TODO: into scope
-    {
-        // this scope is what lets you spawn the handles and uses an unwinding
-        // panic to transmit all of the info back
-    }
 
-    let renderer: Arc<gfx::Renderer> = {
-        let maybe_renderer: Option<Arc<gfx::Renderer>> = crash_handler
-            .create_handle("Renderer Creation".to_string())
-            .enter_oneshot(|| Arc::new(unsafe { gfx::Renderer::new() }));
-
-        crash_handler.handle_crash();
-
-        maybe_renderer.unwrap()
-    };
-
-    let game: Arc<game::Game> = {
-        let maybe_game: Option<Arc<game::Game>> = crash_handler
-            .create_handle("Game Creation".to_string())
-            .enter_oneshot(func);
-
-        crash_handler.handle_crash();
-
-        maybe_game.unwrap()
-    };
-
-    crash_handler
-        .create_handle("Game Tick Thread".to_string())
-        .enter_managed_thread(move || game.tick());
-
-    // renderer.enter_gfx_loop(crash_handler.create_handle("Managed Render
-    // Loop".to_string()));
-
-    // TODO: make not a closure and properly deal with panics
-    // let run = || {
-    //     let renderer = Arc::new(unsafe { gfx::Renderer::new() });
-
-    //     let game = game::Game::new(renderer.clone());
-
-    //     {
-    //         let _verdigris = verdigris::TestScene::new(game.clone());
-
-    //         std::thread::scope(|s| {
-    //             s.spawn(|| game.enter_tick_loop(&should_stop));
-
-    //             renderer.enter_gfx_loop(&should_stop);
-    //         });
-    //     }
-
-    //     if Arc::into_inner(game).is_none()
-    //     {
-    //         log::warn!("Game was retained via Arc cycle! Drop is not
-    // possible");     }
-
-    //     if Arc::into_inner(renderer).is_none()
-    //     {
-    //         log::warn!("Renderer was retained via Arc cycle! Drop is not
-    // possible");     }
-    // };
-
-    // if std::panic::catch_unwind(run).is_err()
-    // {
-    //     should_stop.store(true, SeqCst);
-    // }
-
-    // util::access_global_thread_pool()
-    //     .write()
-    //     .unwrap()
-    //     .take()
-    //     .unwrap()
-    //     .join_threads();
-
-    // logger.stop_worker();
-}
-
-fn spawn_game(
-    crash_handler: Arc<util::CrashHandler>,
-    renderer: Arc<gfx::Renderer>
-) -> Arc<game::Game>
-{
-    let game_lock: Mutex<Option<Arc<game::Game>>> = Mutex::new(None);
-
-    crash_handler
-        .create_handle("Game Creation".to_string())
-        .enter_oneshot(|| {
-            *game_lock.lock().unwrap() = Some(game::Game::new(renderer));
+    crash_handler.into_guarded_scope(|handle| {
+        let renderer = handle.enter_oneshot("Renderer Creation".to_string(), |_| {
+            Arc::new(unsafe { gfx::Renderer::new() })
         });
 
-    game_lock.into_inner().unwrap().unwrap()
+        let game = handle.enter_oneshot("Game Creation".to_string(), || game::Game::new(renderer));
+
+        std::thread::scope(|s| {
+            s.spawn(|| game.enter_tick_loop(&should_stop));
+
+            renderer.enter_gfx_loop(&should_stop);
+        });
+        //     {
+        //         let _verdigris = verdigris::TestScene::new(game.clone());
+
+        //     }
+
+        //     if Arc::into_inner(game).is_none()
+        //     {
+        //         log::warn!("Game was retained via Arc cycle! Drop is not
+        // possible");     }
+
+        //     if Arc::into_inner(renderer).is_none()
+        //     {
+        //         log::warn!("Renderer was retained via Arc cycle! Drop is not
+        // possible");     }
+        // };
+
+        // if std::panic::catch_unwind(run).is_err()
+        // {
+        //     should_stop.store(true, SeqCst);
+        // }
+    });
+
+    crash_handler.finish();
+
+    util::access_global_thread_pool()
+        .write()
+        .unwrap()
+        .take()
+        .unwrap()
+        .join_threads();
+
+    logger.stop_worker();
 }
