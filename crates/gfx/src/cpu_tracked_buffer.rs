e564d7202a8197283f2fc3d52b296ee07ff49325
9291Sam <sam.skinr@gmail.com>
2024-05-16 08:17:03 -0400

    updated cpu tracked buffer

--- crates/gfx/src/cpu_tracked_buffer.rs
+++ crates/gfx/src/cpu_tracked_buffer.rs
@@ -1,31 +1,39 @@
 use std::num::NonZeroU64;
+use std::ops::Deref;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 
-use bytemuck::Zeroable;
+use bytemuck::{bytes_of, cast_slice, AnyBitPattern, NoUninit};
 
 const MAX_FLUSHES_BEFORE_ENTIRE: usize = 128;
 
-pub struct CpuTrackedBuffer<T: Zeroable + Clone>
+pub struct CpuTrackedBuffer<T: AnyBitPattern + NoUninit>
 {
-    renderer:                Arc<super::Renderer>,
-    gpu_data:                wgpu::Buffer,
-    cpu_data_and_flush_list: Mutex<(Box<[T]>, Vec<usize>)>,
-    needs_resize_flush:      AtomicBool
+    renderer:           Arc<super::Renderer>,
+    name:               String,
+    usage:              wgpu::BufferUsages,
+    critical_section:   Mutex<CpuTrackedBufferCriticalSection<T>>,
+    needs_resize_flush: AtomicBool
 }
 
-impl<T: Zeroable + Clone> CpuTrackedBuffer<T>
+struct CpuTrackedBufferCriticalSection<T>
+{
+    gpu_data:   wgpu::Buffer,
+    cpu_data:   Vec<T>,
+    flush_list: Vec<usize>
+}
+
+impl<T: AnyBitPattern + NoUninit> CpuTrackedBuffer<T>
 {
     pub fn new(
         renderer: Arc<super::Renderer>,
         elements: usize,
-        name: &str,
+        name: String,
         usage: wgpu::BufferUsages
     ) -> CpuTrackedBuffer<T>
     {
-        let cpu_data = vec![T::zeroed(); elements].into_boxed_slice();
         let gpu_data = renderer.create_buffer(&wgpu::BufferDescriptor {
-            label: Some(name),
+            label: Some(&name),
             size: elements as u64 * std::mem::size_of::<T>() as u64,
             usage,
             mapped_at_creation: false
@@ -33,18 +41,24 @@ impl<T: Zeroable + Clone> CpuTrackedBuffer<T>
 
         CpuTrackedBuffer {
             renderer,
-            gpu_data,
-            cpu_data_and_flush_list: Mutex::new((cpu_data, Vec::new())),
-            needs_resize_flush: AtomicBool::new(false)
+            critical_section: Mutex::new(CpuTrackedBufferCriticalSection {
+                gpu_data,
+                cpu_data: vec![T::zeroed(); elements],
+                flush_list: Vec::new()
+            }),
+            needs_resize_flush: AtomicBool::new(false),
+            name,
+            usage
         }
     }
 
+    #[allow(clippy::clone_on_copy)]
     pub fn read(&self, index: usize) -> T
     {
-        self.cpu_data_and_flush_list
+        self.critical_section
             .lock()
             .unwrap()
-            .0
+            .cpu_data
             .get(index)
             .expect("Out of Bounds access of CpuTrackedBuffer")
             .clone()
@@ -52,7 +66,11 @@ impl<T: Zeroable + Clone> CpuTrackedBuffer<T>
 
     pub fn write(&self, index: usize, t: T)
     {
-        let (cpu_data, flush_list) = &mut *self.cpu_data_and_flush_list.lock().unwrap();
+        let CpuTrackedBufferCriticalSection {
+            cpu_data,
+            flush_list,
+            ..
+        } = &mut *self.critical_section.lock().unwrap();
 
         if flush_list.len() > MAX_FLUSHES_BEFORE_ENTIRE
         {
@@ -66,63 +84,61 @@ impl<T: Zeroable + Clone> CpuTrackedBuffer<T>
 
     pub fn realloc(&self, elements: usize)
     {
-        let (cpu_data, flush_list) = &mut *self.cpu_data_and_flush_list.lock().unwrap();
+        let CpuTrackedBufferCriticalSection {
+            cpu_data,
+            flush_list,
+            gpu_data
+        } = &mut *self.critical_section.lock().unwrap();
 
-        let mut new_data = vec![T::zeroed(); elements].into_boxed_slice();
+        cpu_data.resize_with(elements, T::zeroed);
 
-        unsafe {
-            std::ptr::copy_nonoverlapping(
-                cpu_data,
-                &mut new_data,
-                cpu_data.len().min(new_data.len())
-            )
-        }
+        *gpu_data = self.renderer.create_buffer(&wgpu::BufferDescriptor {
+            label:              Some(&self.name),
+            size:               cpu_data.len() as u64 * std::mem::size_of::<T>() as u64,
+            usage:              self.usage,
+            mapped_at_creation: false
+        });
 
-        *cpu_data = new_data;
         flush_list.clear();
         self.needs_resize_flush.store(true, Ordering::SeqCst);
     }
 
     pub fn replicate_to_gpu(&self)
     {
-        let (cpu_data, flush_list) = &mut *self.cpu_data_and_flush_list.lock().unwrap();
+        let CpuTrackedBufferCriticalSection {
+            cpu_data,
+            flush_list,
+            gpu_data
+        } = &mut *self.critical_section.lock().unwrap();
 
         if flush_list.len() > MAX_FLUSHES_BEFORE_ENTIRE
             || self.needs_resize_flush.load(Ordering::SeqCst)
         {
             flush_list.clear();
 
-            let buf: *mut T = self
-                .renderer
+            self.renderer
                 .queue
                 .write_buffer_with(
-                    &self.gpu_data,
+                    gpu_data,
                     0,
                     NonZeroU64::new(cpu_data.len() as u64 * std::mem::size_of::<T>() as u64)
                         .unwrap()
                 )
                 .unwrap()
-                .as_mut_ptr() as *mut T;
-
-            unsafe {
-                std::ptr::copy_nonoverlapping::<T>((**cpu_data).as_ptr(), buf, cpu_data.len())
-            };
+                .copy_from_slice(cast_slice(&cpu_data[..]));
         }
         else
         {
             flush_list.drain(..).for_each(|i| {
-                let buf: *mut T = self
-                    .renderer
+                self.renderer
                     .queue
                     .write_buffer_with(
-                        &self.gpu_data,
+                        gpu_data,
                         i as u64 * std::mem::size_of::<T>() as u64,
                         NonZeroU64::new(std::mem::size_of::<T>() as u64).unwrap()
                     )
                     .unwrap()
-                    .as_mut_ptr() as *mut T;
-
-                unsafe { std::ptr::copy_nonoverlapping::<T>(&cpu_data[i], buf, 1) };
+                    .copy_from_slice(cast_slice(bytes_of(&cpu_data[i])));
             });
         }
     }
--- crates/voxel3/src/voxel_manager.rs
+++ crates/voxel3/src/voxel_manager.rs
@@ -1,17 +1,21 @@
-use bytemuck::Zeroable;
+use bytemuck::{AnyBitPattern, NoUninit, Zeroable};
 
 struct VoxelManager
 {
     face_allocator: util::FreelistAllocator,
     face_buffer:    gfx::CpuTrackedBuffer<FaceData>
+    // chunk_data_buffer: gfx::CpuTrackedBuffer<>
 }
 
 impl VoxelManager {}
 
 #[repr(C)]
-#[derive(Clone, Zeroable)]
+#[derive(Clone, Copy, AnyBitPattern, NoUninit)]
 struct FaceData
+// is allocated at a specific index
 {
-    material: u16,
-    chunk_id: u16
+    material:              u16,
+    chunk_id:              u16,
+    location_within_chunk: u32 /* 9 bits each direction | 3 bits normal | 1 bit visibility | 1
+                                * bit unused */
 }
