2b6778d4841f1f788f1c7373fd4ac47267d01e83
9291Sam <sam.skinr@gmail.com>
2024-05-19 15:10:29 -0400

    added spiral iterator for chunk loading

--- crates/verdigris/src/demo_scene.rs
+++ crates/verdigris/src/demo_scene.rs
@@ -116,56 +116,57 @@ fn create_chunk(
 
     let occupied = |x: i32, y: i32, z: i32| (y <= noise_sampler(x, z) as i32);
 
-    let face_iterator = iproduct!(0..511i32, 0..511i32).flat_map(|(local_x, local_z)| {
-        let world_x = (scale * local_x as f64 + offset.x) as i32;
-        let world_z = (scale * local_z as f64 + offset.z) as i32;
+    let face_iterator =
+        spiral::ChebyshevIterator::new(256, 256, 255).flat_map(|(local_x, local_z)| {
+            let world_x = (scale * local_x as f64 + offset.x) as i32;
+            let world_z = (scale * local_z as f64 + offset.z) as i32;
 
-        let noise_h_world = noise_sampler(world_x, world_z);
-        let local_h = (noise_h_world / scale) as i32;
+            let noise_h_world = noise_sampler(world_x, world_z);
+            let local_h = (noise_h_world / scale) as i32;
 
-        ((-4 + local_h)..(local_h + 4)).flat_map(move |sample_h_local| {
-            let sample_h_world = (sample_h_local as f64) * scale;
-            let voxel = if world_x.abs() == 0 || world_z.abs() == 0
-            {
-                0
-            }
-            else
-            {
-                rand::thread_rng().gen_range(1..=12)
-            };
-
-            VoxelFaceDirection::iterate().filter_map(move |d| {
-                if !occupied(world_x, sample_h_world as i32, world_z)
+            ((-4 + local_h)..(local_h + 4)).flat_map(move |sample_h_local| {
+                let sample_h_world = (sample_h_local as f64) * scale;
+                let voxel = if world_x.abs() == 0 || world_z.abs() == 0
                 {
-                    return None;
-                }
-
-                let axis = d.get_axis();
-
-                if occupied(
-                    (world_x as f64 + scale * 1.01 * axis.x as f64).round() as i32,
-                    (sample_h_world + scale * 1.01 * axis.y as f64).round() as i32,
-                    (world_z as f64 + scale * 1.01 * axis.z as f64).round() as i32
-                )
-                {
-                    None
+                    0
                 }
                 else
                 {
-                    Some(VoxelFace {
-                        direction: d,
-                        voxel,
-                        position: glm::I32Vec3::new(
-                            local_x as i32,
-                            (sample_h_world.max(0.0) / scale) as i32,
-                            local_z as i32
-                        ),
-                        material: rand::thread_rng().gen_range(0..=12)
-                    })
-                }
+                    rand::thread_rng().gen_range(1..=12)
+                };
+
+                VoxelFaceDirection::iterate().filter_map(move |d| {
+                    if !occupied(world_x, sample_h_world as i32, world_z)
+                    {
+                        return None;
+                    }
+
+                    let axis = d.get_axis();
+
+                    if occupied(
+                        (world_x as f64 + scale * 1.01 * axis.x as f64).round() as i32,
+                        (sample_h_world + scale * 1.01 * axis.y as f64).round() as i32,
+                        (world_z as f64 + scale * 1.01 * axis.z as f64).round() as i32
+                    )
+                    {
+                        None
+                    }
+                    else
+                    {
+                        Some(VoxelFace {
+                            direction: d,
+                            voxel,
+                            position: glm::I32Vec3::new(
+                                local_x as i32,
+                                (sample_h_world.max(0.0) / scale) as i32,
+                                local_z as i32
+                            ),
+                            material: rand::thread_rng().gen_range(0..=12)
+                        })
+                    }
+                })
             })
-        })
-    });
+        });
 
     for face in face_iterator
     {
