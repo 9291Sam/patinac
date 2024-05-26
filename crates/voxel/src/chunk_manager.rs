ab9365b9aef3e8f15fdbceb2f3f371238e2ee619
9291Sam <sam.skinr@gmail.com>
2024-05-26 08:32:55 -0400

    added ChunkId

--- crates/voxel/src/chunk_manager.rs
+++ crates/voxel/src/chunk_manager.rs
@@ -7,10 +7,13 @@ use gfx::{glm, wgpu};
 
 use crate::{get_world_position_from_chunk, ChunkCoordinate};
 
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) struct ChunkId(pub(crate) u16);
+
 pub(crate) struct ChunkManager
 {
     chunk_id_allocator: util::FreelistAllocator,
-    chunk_pos_id_map:   HashMap<ChunkCoordinate, u16>,
+    chunk_pos_id_map:   HashMap<ChunkCoordinate, ChunkId>,
 
     chunk_data: gfx::CpuTrackedBuffer<GpuChunkData> // TODO: brick map stuff
 }
@@ -33,28 +36,29 @@ impl ChunkManager
         }
     }
 
-    pub fn get_or_insert_chunk(&mut self, chunk_coord: ChunkCoordinate) -> u16
+    pub fn get_or_insert_chunk(&mut self, chunk_coord: ChunkCoordinate) -> ChunkId
     {
         self.get_chunk_id(chunk_coord)
             .unwrap_or_else(|| self.insert_chunk_at(chunk_coord))
     }
 
-    pub fn get_chunk_id(&self, chunk_coord: ChunkCoordinate) -> Option<u16>
+    pub fn get_chunk_id(&self, chunk_coord: ChunkCoordinate) -> Option<ChunkId>
     {
         self.chunk_pos_id_map.get(&chunk_coord).copied()
     }
 
-    pub fn insert_chunk_at(&mut self, chunk_coord: ChunkCoordinate) -> u16
+    pub fn insert_chunk_at(&mut self, chunk_coord: ChunkCoordinate) -> ChunkId
     {
         debug_assert!(self.get_chunk_id(chunk_coord).is_none());
 
-        let new_id = self
-            .chunk_id_allocator
-            .allocate()
-            .expect("Tried to allocate too many chunks");
+        let new_id = ChunkId(
+            self.chunk_id_allocator
+                .allocate()
+                .expect("Tried to allocate too many chunks") as u16
+        );
 
         self.chunk_data.write(
-            new_id,
+            new_id.0 as usize,
             GpuChunkData {
                 position: glm::vec3_to_vec4(&get_world_position_from_chunk(chunk_coord).0.cast()),
                 scale:    glm::vec3_to_vec4(&glm::Vec3::new(1.0, 1.0, 1.0))
@@ -62,11 +66,11 @@ impl ChunkManager
         );
 
         self.chunk_pos_id_map
-            .insert(chunk_coord, new_id as u16)
+            .insert(chunk_coord, new_id)
             .ok_or::<()>(())
             .unwrap_err();
 
-        new_id as u16
+        new_id
     }
 
     pub fn get_buffer<R>(&self, buf_access_func: impl FnOnce(&wgpu::Buffer) -> R) -> R
@@ -87,3 +91,11 @@ struct GpuChunkData
     position: glm::Vec4,
     scale:    glm::Vec4
 }
+
+// struct ChunkCoordinateToIdMap {
+//     data: Vec<(ChunkCoordinate,)
+// }
+
+// impl ChunkCoordinateToIdMap {
+//     pub fn insert(&mut self)
+// }
