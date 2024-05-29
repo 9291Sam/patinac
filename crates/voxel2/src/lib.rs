269866c5b787227fef750f76cf00c500870d4895
9291Sam <sam.skinr@gmail.com>
2024-05-29 10:40:29 -0400

    added suballocated bufer

---
+++ crates/voxel2/src/lib.rs
@@ -0,0 +1,222 @@
+#![feature(new_uninit)]
+use std::fmt::Debug;
+
+use bytemuck::{AnyBitPattern, NoUninit};
+use gfx::glm;
+use offset_allocator::Allocation;
+
+mod suballocated_buffer;
+
+struct Chunk
+{
+    drawable_faces: [Option<(*mut GpuVoxelFace, Allocation)>; 6]
+}
+
+const CHUNK_EDGE_LEN_VOXELS: usize = 256;
+const BRICK_EDGE_LEN_VOXELS: usize = 8;
+const CHUNK_EDGE_LEN_BRICKS: usize = CHUNK_EDGE_LEN_VOXELS / BRICK_EDGE_LEN_VOXELS;
+
+const BRICK_TOTAL_VOXELS: usize = BRICK_EDGE_LEN_VOXELS.pow(3);
+const VISIBILITY_BRICK_U32S_REQUIRED: usize = BRICK_TOTAL_VOXELS / u32::BITS as usize;
+
+#[allow(clippy::assertions_on_constants)]
+const _: () =
+    const { assert!(CHUNK_EDGE_LEN_BRICKS * BRICK_EDGE_LEN_VOXELS == CHUNK_EDGE_LEN_VOXELS) };
+const _: () =
+    const { assert!(VISIBILITY_BRICK_U32S_REQUIRED * u32::BITS as usize == BRICK_TOTAL_VOXELS) };
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct WorldPosition(pub glm::I32Vec3);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub(crate) struct ChunkCoordinate(pub glm::I32Vec3);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Hash)]
+pub(crate) struct ChunkLocalPosition(pub glm::U8Vec3);
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Hash)]
+pub(crate) struct BrickCoordinate(pub glm::U8Vec3);
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Hash)]
+pub(crate) struct BrickLocalPosition(pub glm::U8Vec3);
+
+impl BrickLocalPosition
+{
+    #[inline(never)]
+    pub fn iter() -> &'static [BrickLocalPosition]
+    {
+        const DATA: [BrickLocalPosition; BRICK_TOTAL_VOXELS] = const {
+            let mut out = [BrickLocalPosition(glm::U8Vec3::new(0, 0, 0)); BRICK_TOTAL_VOXELS];
+            let mut idx = 0;
+
+            loop
+            {
+                if idx == BRICK_TOTAL_VOXELS
+                {
+                    break;
+                }
+
+                out[idx] = BrickLocalPosition(glm::U8Vec3::new(
+                    (idx / (BRICK_EDGE_LEN_VOXELS * BRICK_EDGE_LEN_VOXELS)) as u8,
+                    (idx / BRICK_EDGE_LEN_VOXELS) as u8,
+                    (idx % BRICK_EDGE_LEN_VOXELS) as u8
+                ));
+                idx += 1;
+            }
+
+            out
+        };
+
+        &DATA
+    }
+}
+
+impl Debug for WorldPosition
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        write!(f, "[{}, {}, {}]", self.0.x, self.0.y, self.0.z)
+    }
+}
+
+impl Ord for WorldPosition
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering
+    {
+        std::cmp::Ordering::Equal
+            .then(self.0.x.cmp(&other.0.x))
+            .then(self.0.y.cmp(&other.0.y))
+            .then(self.0.z.cmp(&other.0.z))
+    }
+}
+
+impl PartialOrd for WorldPosition
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>
+    {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for ChunkCoordinate
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering
+    {
+        std::cmp::Ordering::Equal
+            .then(self.0.x.cmp(&other.0.x))
+            .then(self.0.y.cmp(&other.0.y))
+            .then(self.0.z.cmp(&other.0.z))
+    }
+}
+
+impl PartialOrd for ChunkCoordinate
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>
+    {
+        Some(self.cmp(other))
+    }
+}
+
+pub(crate) fn world_position_to_chunk_position(
+    WorldPosition(world_pos): WorldPosition
+) -> (ChunkCoordinate, ChunkLocalPosition)
+{
+    (
+        ChunkCoordinate(world_pos.map(|x| x.div_euclid(CHUNK_EDGE_LEN_VOXELS as i32))),
+        ChunkLocalPosition(
+            world_pos
+                .map(|x| x.rem_euclid(CHUNK_EDGE_LEN_VOXELS as i32))
+                .try_cast()
+                .unwrap()
+        )
+    )
+}
+
+pub(crate) fn get_world_offset_of_chunk(
+    ChunkCoordinate(chunk_coordinate): ChunkCoordinate
+) -> WorldPosition
+{
+    WorldPosition(chunk_coordinate.map(|x| x * CHUNK_EDGE_LEN_VOXELS as i32))
+}
+
+pub(crate) fn chunk_local_position_to_brick_position(
+    ChunkLocalPosition(local_chunk_pos): ChunkLocalPosition
+) -> (BrickCoordinate, BrickLocalPosition)
+{
+    (
+        BrickCoordinate(local_chunk_pos.map(|x| x.div_euclid(BRICK_EDGE_LEN_VOXELS as u8))),
+        BrickLocalPosition(
+            local_chunk_pos
+                .map(|x| x.rem_euclid(BRICK_EDGE_LEN_VOXELS as u8))
+                .try_cast()
+                .unwrap()
+        )
+    )
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, AnyBitPattern, NoUninit)]
+struct GpuVoxelFace
+{
+    // [0,  7]  - x pos
+    // [8, 15]  - y pos
+    // [16, 23] - z pos
+    // [24, 27] - breadth width
+    // [28, 31] - breadth height
+    data: u32
+}
+
+impl GpuVoxelFace
+{
+    pub fn new(ChunkLocalPosition(pos): ChunkLocalPosition, breadth: glm::U8Vec2) -> Self
+    {
+        debug_assert!(breadth.x < 16);
+        debug_assert!(breadth.y < 16);
+
+        GpuVoxelFace {
+            data: pos.x as u32
+                | (pos.y as u32) << 8
+                | (pos.z as u32) << 16
+                | (breadth.x as u32) << 24
+                | (breadth.y as u32) << 28
+        }
+    }
+
+    pub fn destructure(self) -> (ChunkLocalPosition, glm::U8Vec2)
+    {
+        (
+            ChunkLocalPosition(glm::U8Vec3::new(
+                (self.data & 0b1111_1111) as u8,
+                ((self.data >> 8) & 0b1111_1111) as u8,
+                ((self.data >> 16) & 0b1111_1111) as u8
+            )),
+            glm::U8Vec2::new(
+                ((self.data >> 24) & 0b1111) as u8,
+                ((self.data >> 28) & 0b1111) as u8
+            )
+        )
+    }
+}
+
+#[cfg(test)]
+mod test
+{
+    use gfx::glm;
+    use itertools::iproduct;
+
+    use super::GpuVoxelFace;
+
+    #[test]
+    pub fn test_construct_destroy()
+    {
+        for (x, y, z, w, h) in iproduct!(0..=255, 0..=255, 0..=255, 0..=15, 0..=15)
+        {
+            let (p, b) = GpuVoxelFace::new(
+                crate::ChunkLocalPosition(glm::U8Vec3::new(x, y, z)),
+                glm::U8Vec2::new(w, h)
+            )
+            .destructure();
+
+            assert!(p.0.x == x);
+            assert!(p.0.y == y);
+            assert!(p.0.z == z);
+            assert!(b.x == w);
+            assert!(b.y == h);
+        }
+    }
+}
---
+++ crates/voxel2/src/suballocated_buffer.rs
@@ -0,0 +1,304 @@
+use std::hash::Hash;
+use std::num::NonZeroU64;
+use std::ops::Range;
+use std::sync::Arc;
+
+use bytemuck::{cast_slice, Pod};
+use gfx::wgpu;
+pub use offset_allocator::{Allocation as OAllocation, Allocator as OAllocator};
+
+struct SubAllocatedCpuTrackedBuffer<T: Pod>
+{
+    renderer:  Arc<gfx::Renderer>,
+    allocator: OAllocator<u32>,
+
+    gpu_buffer: wgpu::Buffer,
+    cpu_buffer: Box<[T]>,
+
+    flush_ranges: Vec<RangeInclusive<u32>>
+}
+
+struct BufferAllocation
+{
+    internal_allocation: OAllocation,
+    length:              u32
+}
+
+impl Hash for BufferAllocation
+{
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H)
+    {
+        self.internal_allocation.offset.hash(state);
+        self.length.hash(state);
+    }
+}
+
+impl BufferAllocation
+{
+    fn to_valid_range(&self) -> Range<u32>
+    {
+        self.internal_allocation.offset..(self.internal_allocation.offset + self.length)
+    }
+}
+
+impl<T: Pod> SubAllocatedCpuTrackedBuffer<T>
+{
+    pub fn new(
+        renderer: Arc<gfx::Renderer>,
+        capacity: u32,
+        buffer_label: &str,
+        usages: wgpu::BufferUsages
+    ) -> Self
+    {
+        SubAllocatedCpuTrackedBuffer {
+            renderer:     renderer.clone(),
+            allocator:    OAllocator::new(capacity),
+            gpu_buffer:   renderer.create_buffer(&wgpu::BufferDescriptor {
+                label:              Some(buffer_label),
+                size:               capacity as u64 * std::mem::size_of::<T>() as u64,
+                usage:              usages,
+                mapped_at_creation: false
+            }),
+            cpu_buffer:   unsafe { Box::new_zeroed_slice(capacity as usize).assume_init() },
+            flush_ranges: Vec::new()
+        }
+    }
+
+    pub fn allocate(&mut self, length: u32) -> BufferAllocation
+    {
+        let internal_allocation = self.allocator.allocate(length).unwrap_or_else(|| {
+            panic!(
+                "Allocation of {} {}s failed!",
+                length,
+                std::any::type_name::<T>()
+            )
+        });
+        BufferAllocation {
+            internal_allocation,
+            length
+        }
+    }
+
+    pub fn realloc(&mut self, new_length: u32, old_allocation: BufferAllocation)
+    -> BufferAllocation
+    {
+        if new_length
+            <= self
+                .allocator
+                .allocation_size(old_allocation.internal_allocation)
+        {
+            BufferAllocation {
+                internal_allocation: old_allocation.internal_allocation,
+                length:              new_length
+            }
+        }
+        else
+        {
+            let old_data_range = old_allocation.to_valid_range();
+            self.allocator.free(old_allocation.internal_allocation);
+
+            let new_allocation_internal =
+                self.allocator.allocate(new_length).unwrap_or_else(|| {
+                    panic!(
+                        "Allocation of {} {}s failed!",
+                        new_length,
+                        std::any::type_name::<T>()
+                    )
+                });
+
+            let new_allocation = BufferAllocation {
+                internal_allocation: new_allocation_internal,
+                length:              new_length
+            };
+
+            self.cpu_buffer.copy_within(
+                (old_data_range.start as usize)..(old_data_range.end as usize),
+                new_allocation.to_valid_range().start as usize
+            );
+
+            let new_allocation_range = new_allocation.to_valid_range();
+
+            self.flush_ranges
+                .push(new_allocation_range.start..=new_allocation_range.end - 1);
+
+            new_allocation
+        }
+    }
+
+    pub fn deallocate(&mut self, allocation: BufferAllocation)
+    {
+        self.allocator.free(allocation.internal_allocation)
+    }
+
+    pub fn read(&self, allocation: &BufferAllocation, local_range: Range<u32>) -> &[T]
+    {
+        let global_range = self.get_allocation_range(allocation, local_range);
+
+        &self.cpu_buffer[(global_range.start as usize)..(global_range.end as usize)]
+    }
+
+    pub fn write(&mut self, allocation: &BufferAllocation, local_range: Range<u32>, data: &[T])
+    {
+        debug_assert!(local_range.len() == data.len());
+
+        let global_range = self.get_allocation_range(allocation, local_range);
+
+        self.cpu_buffer[(global_range.start as usize)..(global_range.end as usize)]
+            .copy_from_slice(data);
+
+        self.flush_ranges
+            .push(global_range.start..=(global_range.end - 1))
+    }
+
+    pub fn access_ref<K>(
+        &self,
+        allocation: &BufferAllocation,
+        local_range: Range<u32>,
+        func: impl FnOnce(&[T]) -> K
+    ) -> K
+    {
+        func(self.read(allocation, local_range))
+    }
+
+    pub fn access_mut<K>(
+        &mut self,
+        allocation: &BufferAllocation,
+        local_range: Range<u32>,
+        func: impl FnOnce(&mut [T]) -> K
+    ) -> K
+    {
+        let global_range = self.get_allocation_range(allocation, local_range);
+
+        let k =
+            func(&mut self.cpu_buffer[(global_range.start as usize)..(global_range.end as usize)]);
+
+        self.flush_ranges
+            .push(global_range.start..=(global_range.end - 1));
+
+        k
+    }
+
+    pub fn access_buffer(&self) -> &wgpu::Buffer
+    {
+        &self.gpu_buffer
+    }
+
+    pub fn replicate_to_gpu(&mut self)
+    {
+        let flush_ranges = combine_into_ranges(
+            std::mem::replace(&mut self.flush_ranges, Vec::new()),
+            2u32.pow(14),
+            32
+        );
+
+        for range in flush_ranges
+        {
+            let range_len_elements = range.end() + 1 - range.start();
+
+            let usize_range = (*range.start() as usize)..=(*range.end() as usize);
+
+            if range_len_elements > 0
+            {
+                self.renderer
+                    .queue
+                    .write_buffer_with(
+                        &self.gpu_buffer,
+                        *range.start() as u64 * std::mem::size_of::<T>() as u64,
+                        NonZeroU64::new(
+                            range_len_elements as u64 * std::mem::size_of::<T>() as u64
+                        )
+                        .unwrap()
+                    )
+                    .unwrap()
+                    .copy_from_slice(cast_slice(&self.cpu_buffer[usize_range]))
+            }
+        }
+    }
+
+    fn get_allocation_range(
+        &self,
+        allocation: &BufferAllocation,
+        local_range: Range<u32>
+    ) -> Range<u32>
+    {
+        let front = allocation.internal_allocation.offset + local_range.start;
+        let back = allocation.internal_allocation.offset + local_range.end;
+        let read_range = front..back;
+
+        #[cfg(debug_assertions)]
+        {
+            let allocated_range = allocation.to_valid_range();
+
+            debug_assert!(read_range.start >= allocated_range.start);
+            debug_assert!(read_range.end <= allocated_range.end);
+        }
+
+        read_range
+    }
+}
+
+use std::ops::RangeInclusive;
+
+pub(crate) fn combine_into_ranges(
+    mut ranges: Vec<RangeInclusive<u32>>,
+    max_distance: u32,
+    max_ranges: usize
+) -> Vec<RangeInclusive<u32>>
+{
+    if ranges.is_empty()
+    {
+        return vec![];
+    }
+
+    // Sort ranges by their start value
+    ranges.sort_unstable_by_key(|range| *range.start());
+
+    let mut combined_ranges = Vec::with_capacity(ranges.len());
+    let mut current_range = unsafe { ranges.get_unchecked(0).clone() };
+
+    for i in 1..ranges.len()
+    {
+        let range = unsafe { ranges.get_unchecked(i) };
+        if *range.start() <= *current_range.end() + max_distance
+        {
+            current_range = *current_range.start()..=*range.end().max(current_range.end());
+        }
+        else
+        {
+            combined_ranges.push(current_range);
+            current_range = range.clone();
+        }
+    }
+    combined_ranges.push(current_range);
+
+    unsafe {
+        if combined_ranges.len() > max_ranges
+        {
+            while combined_ranges.len() > max_ranges
+            {
+                let mut min_distance = u32::MAX;
+                let mut min_index = 0;
+
+                for i in 0..combined_ranges.len() - 1
+                {
+                    let distance = *combined_ranges.get_unchecked(i + 1).start()
+                        - *combined_ranges.get_unchecked(i).end();
+                    if distance < min_distance
+                    {
+                        min_distance = distance;
+                        min_index = i;
+                    }
+                }
+
+                let merged_range = *combined_ranges.get_unchecked(min_index).start()
+                    ..=*combined_ranges.get_unchecked(min_index + 1).end();
+                *combined_ranges.get_unchecked_mut(min_index) = merged_range;
+                combined_ranges.remove(min_index + 1);
+            }
+
+            combined_ranges.set_len(max_ranges);
+        }
+    }
+
+    combined_ranges
+}
